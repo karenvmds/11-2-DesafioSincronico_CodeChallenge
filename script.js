b4395d61b0fd6a573823c6b8dcc8be0f0172f78c
karenvmds <141439738+karenvmds@users.noreply.github.com>
2023-09-08 13:08:25 -0300

    Actualización del código

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ formulario.addEventListener("submit", function (event) {
     
     event.preventDefault();
 
-    const formDatos = new FormData(this);
+    // const formDatos = new FormData(this);
+
+    var nombre = document.getElementById("nombres").value;
+    var apellido = document.getElementById("apellidos").value;
+    var nacimiento = document.getElementById("nacimiento").value;
+
 
     fetch("https://jsonplaceholder.typicode.com/users", {
         method: "POST",
-        body: formDatos,
-    }).then(response => response.json())
-    .then(resServidor=>{
-       console.log(resServidor);
-
-})
-})
+        body: JSON.stringify({
+            nombre: nombre,
+            apellido: apellido,
+            fecha: nacimiento,
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+       
+    }).then(response => response.json()).then(datos => console.log(datos))
+    })
